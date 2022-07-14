e3af3a6869a749a6e6c3b24a18a0088fae76f4f9
EllaLi12 <104800997+EllaLi12@users.noreply.github.com>
2022-07-14 15:09:43 -0500

    Update Organized User-defined functions.R

--- Organized Codes/Organized User-defined functions.R
+++ Organized Codes/Organized User-defined functions.R
@@ -1,56 +1,61 @@
+library("nortest")
+library("dgof")
+library("dplyr")
+library(moments)
+rm(list=ls())
 
-#Organized TypeI Error Test
-
-#Same Mean
-alpha <- 0.05
-N <- 1000
-rejectH0 <- pval <-  numeric(N)
-
-nvec <- c(5,10,15,20,25)
-dfvec <- c(3,5,8,10,15)
-powervec <- numeric(length(nvec)*length(dfvec))
-
-#matrix instead[i,k]
-m <- 1
-
-for(i in 1:length(nvec)){
-  n <- nvec[i]
-  for(k in 1:length(dfvec)){
-    df <- dfvec[k]
-    powermatrixs <- matrix(powervec,
-                          nrow=length(nvec),ncol=length(dfvec),byrow=TRUE)
-    for(j in 1:N){
-      x <- generate_data(n,"Chi-square",df)
-      y <- generate_data(n,"Chi-square",df)
-      out <- t.test(x,y)
-      pval <- out$p.value
-      if(pval < alpha){
-        rejectH0[j] <- 1
-      }
-    }
-    powermatrixs[i,k] <- mean(rejectH0)
+#User-defined functions
+#Generate data from different distribution
+generate_data <- function(n, dist,par1, par2){
+  if(dist == "Normal"){
+    x <- rnorm(n, mean = par1, sd = par2)
   }
-}
-powermatrixs
-
-
-#Different Mean
-for(i in 1:length(nvec)){
-  n <- nvec[i]
-  for(k in 1:length(dfvec)){
-    df <- dfvec[k]
-    powermatrixd <- matrix(powervec,
-                          nrow=length(nvec),ncol=length(dfvec),byrow=TRUE)
-    for(j in 1:N){
-      x <- generate_data(n,"Chi-square",df)
-      y <- generate_data(n,"Chi-square",20)
-      out <- t.test(x,y)
-      pval <- out$p.value
-      if(pval < alpha){
-        rejectH0[j] <- 1
-      }
-    }
-    powermatrixd[i,k] <- mean(rejectH0)
+  if(dist == "Standard Normal"){
+    x <- rnorm(n, mean = 0, sd = 1)
   }
+  if(dist == "Chi-square"){
+    x <- rchisq(n,par1)
+  }
+  if(dist == "Gama"){
+    x <- rgamma(n, shape = 2, rate = 1)
+  }
+  if(dist == "Beta"){
+    x <- rbeta(n, shape1 = 2, shape2 = 5)
+  }
+  if(dist == "Expotential"){
+    x <- rexp(n, rate = 1)
+  }
+  if(dist == "t"){
+    x <- rt(n,df = par1)
+  }
+  if(dist == "Uniform"){
+    x <- runif(n, min = 40,max = 60)
+  }
+  
+  return(x)
 }
-powermatrixd
+
+x <- generate_data(5, "Normal", 100, 2)
+x
+y <- generate_data(5, "Standard Normal")
+y
+
+
+#Apply different tests
+generate_tests <- function(x, test){
+  if(test == "KS"){
+    output <- lillie.test(x)
+  }
+  if(test == "SW"){
+    output <- shapiro.test(x)
+  }
+  if(test == "JB"){
+    output <- jarque.test(x)
+  }
+  if(test == "DAP"){
+    output <- agostino.test(x)
+  }
+  return(output)
+}
+output <- generate_tests(x,"KS")
+output
