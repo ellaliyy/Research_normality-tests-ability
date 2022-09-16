cf1aadf8dddb56f7ff6c8eaf54bd88b45b802a2c
EllaLi12 <104800997+EllaLi12@users.noreply.github.com>
2022-09-16 13:16:04 -0500

    Update Organized User-defined functions.R

--- Organized Codes/Organized User-defined functions.R
+++ Organized Codes/Organized User-defined functions.R
@@ -5,32 +5,31 @@ library(moments)
 
 #User-defined functions
 
-#Generate data from different distribution
-generate_data <- function(n, dist,par1, par2){
+#Generate data from different distribution but located similarly
+generate_data <- function(n, dist, par1){
   if(dist == "Normal"){
-    x <- rnorm(n, mean = par1, sd = par2)
+    x <- rnorm(n, mean = par1, sd = par1)
   }
   if(dist == "Standard Normal"){ 
-    #This becomes redundant when you have par1 and par2 above 
-    x <- rnorm(n, mean = 0, sd = 1)
+    x <- rnorm(n, mean = 0, sd = 1)+par1
   }
   if(dist == "Chi-square"){
-    x <- rchisq(n, par1) #may be set a default for par1?
+    x <- rchisq(n, par1)
   }
   if(dist == "Gamma"){
-    x <- rgamma(n, shape = par1, rate = par2)
+    x <- rgamma(n, shape = (par1^2), rate = par1)
   }
   if(dist == "Beta"){
-    x <- rbeta(n, shape1 = par1, shape2 = par2)
+    x <- rbeta(n, shape1 = par1 , shape2 = par1)+9.5
   }
   if(dist == "Expotential"){
-    x <- rexp(n, rate = par1)
+    x <- rexp(n, rate = 1/par1)
   }
   if(dist == "t"){
-    x <- rt(n,df = par1) #may be set a default for par1?
+    x <- rt(n,df = par1)+par1
   }
   if(dist == "Uniform"){
-    x <- runif(n, min = par1,max = par2)
+    x <- runif(n, min = (par1-10),max = (par1+10))
   }
   
   return(x)
@@ -52,3 +51,4 @@ generate_tests <- function(x, test){
   }
   return(output)
 }
+
